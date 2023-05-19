a5fb155d45298da422bbd2a383f9bafdf5e2c7c4
afr114 <afr114@gmail.com>
2023-05-19 11:49:27 -0700

    add neo-tree toggle

--- lua/plugins-config/init.lua
+++ lua/plugins-config/init.lua
@@ -1,3 +1,4 @@
 require('plugins-config.telescope')
 require('plugins-config.lsp-config')
 require('plugins-config.vim-test')
+require('plugins-config.neo-tree')
---
+++ lua/plugins-config/neo-tree.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap("n", "<F2>", ":NeoTreeShowToggle<CR>", {noremap = true})
+
