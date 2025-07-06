b64b58f95473e0160d53d8d6a96a0f5121c72d95
afr114 <afr114@gmail.com>
2025-07-05 22:58:47 -0700

    add new lsp for psql using postgres language server

--- lua/plugins-config/lsp-config.lua
+++ lua/plugins-config/lsp-config.lua
@@ -1,35 +1,33 @@
 local has_words_before = function()
-   local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-    if col == 0 then
-        return false
-    end
-    local line_text = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-    return not line_text:sub(col, col):match("%s")
+  local pos = vim.api.nvim_win_get_cursor(0)
+  local line, col = pos[1], pos[2]
+
+  if col == 0 then
+    return false
+  end
+
+  local line_text = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+  return not line_text:sub(col, col):match("%s")
 end
 
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 
-require("lspconfig").sourcekit.setup{
-  cmd = {'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp'}
-}
+-- Default on_attach function
+local on_attach = function(client, bufnr)
+  -- Add buffer-local keymaps or customizations here
+end
 
-
+-- Setup nvim-cmp
 cmp.setup({
-
-snippet = {
-  -- REQUIRED - you must specify a snippet engine
-  expand = function(args)
-    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-  end,
-},
-
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
 
   sources = {
-    { name = "minuet"},
+    { name = "minuet" },
     { name = "nvim_lua" },
     { name = "nvim_lsp" },
     { name = "path" },
@@ -37,11 +35,10 @@ snippet = {
     { name = "buffer", keyword_length = 1 },
   },
 
-
   mapping = {
-   ['<cr>'] = cmp.mapping.confirm({select = true}),
+    ["<cr>"] = cmp.mapping.confirm({ select = true }),
 
-     ["<up>"] = cmp.mapping(function(fallback)
+    ["<up>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -51,7 +48,7 @@ snippet = {
       end
     end, { "i", "s" }),
 
-  ["<down>"] = cmp.mapping(function(fallback)
+    ["<down>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -62,108 +59,108 @@ snippet = {
         fallback()
       end
     end, { "i", "s" }),
-
-	}
+  },
 })
 
+-- LSP setup
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig.configs")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- Load the lspconfig module
-local nvim_lsp = require('lspconfig')
+-- Register Postgres Language Server (for older lspconfig versions)
+if not configs.postgrestools then
+  configs.postgrestools = {
+    default_config = {
+      cmd = { "postgrestools", "lsp-proxy" },
+      filetypes = { "sql" },
+      root_dir = lspconfig.util.root_pattern("postgrestools.jsonc", ".git"),
+      single_file_support = true,
+    },
+  }
+end
 
--- Create a capabilities object to pass to the LSP client with snippet support
-local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
--- Setup tsserver language server with specified filetypes, root directory, and on_attach function
-nvim_lsp.ts_ls.setup{
+-- Use built-in if available
+lspconfig.postgres_lsp.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- TypeScript / JavaScript
+lspconfig.ts_ls.setup({
+  on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  root_dir = nvim_lsp.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-  on_attach = function(client, bufnr)
-    -- will be called when the language server is attached to a buffer.
-    -- This is a good place to put any additional configuration or customization 
-    -- that you want to do, specific to this language server.
-  end,
-}
+  root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+})
 
-nvim_lsp.gopls.setup{
+-- Go
+lspconfig.gopls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = { "gopls"},
+  cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = nvim_lsp.util.root_pattern("go.work", "go.mod")
-}
+  root_dir = lspconfig.util.root_pattern("go.work", "go.mod"),
+})
 
-nvim_lsp.solargraph.setup{
-  capabilities = capabilities,
-  filtetype = { "ruby"}
-}
-
-nvim_lsp.sourcekit.setup{
+-- Ruby
+lspconfig.solargraph.setup({
   on_attach = on_attach,
-  cmd = {"xcrun", "sourcekit-lsp"};
-  filetypes = {"swift", "c", "cpp", "objc", "objcpp"};
-  root_dir = nvim_lsp.util.root_pattern("compile_commands.json", ".git");
-  capabilities = capabilities;
-}
+  capabilities = capabilities,
+  filetypes = { "ruby" },
+})
 
--- Equivalent of the autocmd for swift files
--- vim.cmd [[
---   autocmd FileType swift autocmd BufWritePost *.swift :silent exec "!swiftformat %"
--- ]]
+-- Swift / C / C++ / Objective-C
+lspconfig.sourcekit.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "xcrun", "sourcekit-lsp" },
+  filetypes = { "swift", "c", "cpp", "objc", "objcpp" },
+  root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
+})
 
--- Enable completion for TypeScript and JavaScript
+-- Enable omnifunc for TypeScript and JavaScript
 vim.cmd("autocmd FileType typescript,javascript setlocal omnifunc=v:lua.vim.lsp.omnifunc")
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+-- Global diagnostic keymaps
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+-- LspAttach autocmd to set buffer-local keymaps
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
+
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format({ async = true })
     end, opts)
   end,
 })
 
--- vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
--- vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
---
+-- Diagnostic display settings
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- Enable signs
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
     signs = true,
-    -- Enable virtual text
     virtual_text = true,
-    -- Show line diagnostics on hover
     update_in_insert = false,
   }
 )
-
 
