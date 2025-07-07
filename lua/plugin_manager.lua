24508b2e3f1113d320be5eb370425f96e9545173
afr114 <afr114@gmail.com>
2025-07-07 11:18:18 -0700

    add mardown and hint

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -285,6 +285,30 @@ local plugins = {
 
   { "kelly-lin/telescope-ag", dependencies = { "nvim-telescope/telescope.nvim" } },
 
+  {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+},
+
   -- Clipboard
   {
     "EtiamNullam/deferred-clipboard.nvim",
