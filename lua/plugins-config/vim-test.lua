bd0ef8be42d0cc57264ae503c54cc11602b8726b
afr114 <afr114@gmail.com>
2026-03-19 22:07:10 -0700

    fix tj jest tests

--- lua/plugins-config/vim-test.lua
+++ lua/plugins-config/vim-test.lua
@@ -3,5 +3,4 @@ vim.api.nvim_set_keymap('n', '/s', ':TestSuite<CR>', { silent = true})
 vim.api.nvim_set_keymap('n', '/l', ':TestLast<CR>', {silent = true})
 vim.api.nvim_set_keymap('n', '/v', ':TestVisit<CR>', { silent = true})
 vim.api.nvim_set_keymap('n', '/f', ':TestFile<CR>', { silent = true})
-vim.g.test_javascript_jest_executable = 'yarn tj'
-
+vim.g["test#javascript#jest#executable"] = 'yarn tj'
